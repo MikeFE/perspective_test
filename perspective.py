f87893390eab4bb0dd0355cb76b0cc56572381f6
M <moonshine@gmail.com>
2017-10-28 10:11:46 +0000

    Add additional statistics when judging text msgs

--- perspective.py
+++ perspective.py
@@ -8,25 +8,54 @@ import json
 
 PERSPECTIVE_URL = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key={}'.format(API_KEY)
 
+enum_to_str_stats = {
+    'TOXICITY': 'Toxicity',
+    'ATTACK_ON_AUTHOR': 'Attack on author',
+    'ATTACK_ON_COMMENTER': 'Attack on commenter',
+    'INCOHERENT': 'Incorrehence',
+    'INFLAMMATORY': 'Inflammatory',
+    'LIKELY_TO_REJECT': 'Likely to be rejected by mod',
+    'OBSCENE': 'Obscenity',
+    'SEVERE_TOXICITY': 'Severe toxicity',
+    'SPAM': 'Potential spam',
+    'UNSUBSTANTIAL': 'Ubsubstantial comments'
+}
+
 def get_toxicity(text):
     data = {
         'comment': {'text': text},
         'languages': ['en'],
-        'requestedAttributes': {'TOXICITY': {}}
+        'requestedAttributes': {
+            'TOXICITY': {},
+            'ATTACK_ON_AUTHOR': {},
+            'ATTACK_ON_COMMENTER': {},
+            'INCOHERENT': {},
+            'INFLAMMATORY': {},
+            'LIKELY_TO_REJECT': {},
+            'OBSCENE': {},
+            'SEVERE_TOXICITY': {},
+            'SPAM': {},
+            'UNSUBSTANTIAL': {}
+        }
     }
 
     r = requests.post(PERSPECTIVE_URL, json=data)
     try:
-        return r.json()['attributeScores']['TOXICITY']['summaryScore']['value']
+        stats = {}
+        for attrib in r.json()['attributeScores'].keys():
+            stats[attrib] = r.json()['attributeScores'][attrib]['summaryScore']['value']
+        return stats
     except KeyError:
         print(r.text)
         return 'error'
+
 
 def main():
     try:
         while 1:
             text = input('Enter text: ')
-            print('Toxicity score: {}'.format(get_toxicity(text)))
+            for k, v in get_toxicity(text).items():
+                print('\t{} estimate: {:.1%}'.format(enum_to_str_stats[k], v))
     except KeyboardInterrupt:
         print()
         return
